3c80429b99d0db8cc4eb9b8bf732f0dff9f8a858
Pallér Péter <paller.peter@ibtconsulting.hu>
2024-02-21 14:02:44 +0100

    desktop v_1 done

--- Biobank/src/app/home-page/features/features.component.ts
+++ Biobank/src/app/home-page/features/features.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 export class FeaturesComponent {
 
   specimen: any;
-  aliquots: any[];
+  aliquot: any;
+  report: any;
+  userFunctions: any;
 
   responsiveOptions: any[] | undefined;
 
@@ -16,28 +18,34 @@ export class FeaturesComponent {
 
     this.specimen = {
       title: "Traceability of each specimen and aliquot through their entire lifespan",
-      description: `Seamless integration to various input data sources (e.g. spreadsheets, database platforms, or other data management systems) ensuring the ability to continuously evolving biobank needs.`,
+      description: `Seamless integration to various input data sources (e.g. spreadsheets, database platforms, or other data management systems) 
+                    ensuring the ability to continuously evolving biobank needs.`,
       images: [{ name: 'specimen_list.png' }, { name: 'specimen_clinical_assessment.png' }, { name: 'specimen_clinical_diagnosis.png'}]
-     };
+    };
 
-    this.aliquots = [
-      {
-      title: "Traceability of each specimen and aliquot through their entire lifespan",
-      description: `The modular nature of our solution allows for scalability and flexibility, making it possible to integrate new functionalities and technologies as they emerge.
-              This ensures that our clients' systems remain at the cutting edge of digital health innovation, adaptable to evolving healthcare demands and technological advancements.`,
-      image: 'specimen_list.png',
-     },
-     {
-      description: 'Seamless integration to various input data sources (e.g. spreadsheets, database platforms, or other data management systems) ensuring the ability to continuously evolving biobank needs.',
-      image: 'specimen_clinical_assessment.png',
+    this.aliquot = {
       title: "Centralized storage management",
-     },
-     {
-      description: 'Description for feature 3, description for feature 3, description for feature 3',
-      image: 'specimen_clinical_diagnosis.png',
+      description: `Centralized storage facility, sample-and aliquot management across multiple locations 
+                    (in a multi-centric settings single country, or cross-countries) all in one user-friendly platform `,
+      images: [{ name: 'aliquot_storage_map.png' }, { name: 'aliquot_assignment.png' }, { name: 'aliquot_events.png'}]
+    };
+
+    this.report = {
       title: "Structured data organized in various modules provide wide range of insights",
-     }
-    ];
+      description: `The integrated dashboards offer a comprehensive view of various biobanking aspects, based on specific user roles. 
+                    Deep dive analysis of anonymised clinical data provides a solid basis for processing big clinical data for medical research or clinical studies.`,
+      images: [{ name: 'admin_report_aliquot.png' }, { name: 'mgmt_report.png' }]
+    };
+
+    this.userFunctions = {
+      title: "Interoperability with high level of security ",
+      description: `The robust structure of the system provides a high degree of availability and security for all users. 
+                    The system operates under stern user management policy and authorization control. 
+                    Full and retaining activity-logging ensures a high level of protection,
+                    ultimately enhancing the effectiveness and integrity of biobank operations in the modern scientific landscape. 
+                    Fast FHIR-HL7/OMOP-CDM conversions foster interoperability with external databases following standardised data models.`,
+      images: [{ name: 'usr_mgn.png' }, { name: 'pers_data.png' }, { name: 'sys_arch.png' }]
+    };
 
     this.responsiveOptions = [
       {
