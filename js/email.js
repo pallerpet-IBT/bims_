0fa7d6daf6f1122dfeeb30c77574ec0440151756
Pallér Péter <paller.peter@ibtconsulting.hu>
2024-02-22 16:27:06 +0100

    layout, testimonial

---
+++ js/email.js
@@ -0,0 +1,20 @@
+
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+    publicKey: "8MIUeQwrJghC6u4Xl"
+    });
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_uiedg7x', 'template_o1jsn3m', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
